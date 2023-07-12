f524e88ff5adfeaacceda0e5a9497fd2cc5f504c
Marc kender Romel Jean Charles <makender103@gmail.com>
2023-07-12 15:38:58 -0300

    chore: popular section 100%

--- Project/front-end/src/components/template/Home/PopularProducts/index.tsx
+++ Project/front-end/src/components/template/Home/PopularProducts/index.tsx
@@ -16,13 +16,40 @@ interface CardCarouselProps {
 
 const PopularProducts: React.FC<CardCarouselProps> = ({ cards }) => {
   const settings = {
-    dots:false,
+    dots:true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
     arrows: false,
     autoplay: false,
     autoplaySpeed: 5000,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
   };
 
   return (
