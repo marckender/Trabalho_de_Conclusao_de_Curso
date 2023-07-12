cf4203a0200d43ac430ca87a7ddbfb93bb9cfa1e
Marc kender Romel Jean Charles <makender103@gmail.com>
2023-07-12 15:19:30 -0300

    feat: afrom home

--- Project/front-end/src/components/pages/Home/Home/index.tsx
+++ Project/front-end/src/components/pages/Home/Home/index.tsx
@@ -1,10 +1,38 @@
 import Hero from "../../../template/Home/Hero";
+import PopularProducts from "../../../template/Home/PopularProducts";
 import { PageDefault } from "../PageDefault";
 
+
 export default function HomePage() {
+  const cards = [
+    {
+      title: 'Card 1',
+      image: 'https://i.imgur.com/lYKm8Wf.png',
+      description: 'Description for Card 1',
+    },
+    {
+      title: 'Card 2',
+      image: 'https://i.imgur.com/lYKm8Wf.png',
+      description: 'Description for Card 2',
+    },
+    {
+      title: 'Card 3',
+      image: 'https://i.imgur.com/lYKm8Wf.png',
+      description: 'Description for Card 3',
+    },
+    {
+      title: 'Card 4',
+      image: 'https://i.imgur.com/lYKm8Wf.png',
+      description: 'Description for Card 4',
+    },
+  ];
   return (
     <PageDefault>
       <Hero />
+      <br />
+      <PopularProducts cards={cards} />
+    
+    <br />
     </PageDefault>
   )
 }
---
+++ Project/front-end/src/components/template/Home/PopularProducts/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './styles.scss';
+
+interface Card {
+  title: string;
+  image: string;
+  description: string;
+}
+
+interface CardCarouselProps {
+  cards: Card[];
+}
+
+const PopularProducts: React.FC<CardCarouselProps> = ({ cards }) => {
+  const settings = {
+    dots:false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 5000,
+  };
+
+  return (
+    <div className="card-carousel">
+        <h1>Popular Product</h1>
+      <Slider {...settings}>
+        {cards.map((card, index) => (
+          <div key={index} className="card">
+            <img src={card.image} alt={card.title} />
+            <h3>#20.000 <span>$5.00</span></h3>
+            <p>{card.description}</p>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default PopularProducts;
