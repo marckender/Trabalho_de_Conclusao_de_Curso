19da218d20d87d55db01597ece9ba7c562c81fca
Marc kender Romel Jean Charles <makender103@gmail.com>
2023-07-18 15:56:24 -0300

    chore: vercel deploy error

--- Project/front-end/src/components/pages/Home/Home/index.tsx
+++ Project/front-end/src/components/pages/Home/Home/index.tsx
@@ -9,30 +9,6 @@ import { useProductContext } from "../../../../contexts/useProductContext";
 export default function HomePage() {
 
   const {products, getProducts} = useProductContext()
-
-  const cards = [
-    {
-      title: 'Card 1',
-      image: 'https://i.imgur.com/lYKm8Wf.png',
-      description: 'Kinky Straight Short Bob Frontal Lace Wig',
-    },
-    {
-      title: 'Card 2',
-      image: 'https://i.imgur.com/lYKm8Wf.png',
-      description: 'Kinky Straight Short Bob Frontal Lace Wig',
-    },
-    {
-      title: 'Card 3',
-      image: 'https://i.imgur.com/lYKm8Wf.png',
-      description: 'Kinky Straight Short Bob Frontal Lace Wig',
-    },
-    {
-      title: 'Card 4',
-      image: 'https://i.imgur.com/lYKm8Wf.png',
-      description: 'Kinky Straight Short Bob Frontal Lace Wig',
-    },
-  ];
-
   useEffect(() => {
     getProducts();
   }, [])
