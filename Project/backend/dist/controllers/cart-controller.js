b880b1d3cfde5081675a16fc1942f34bd9dce399
Marc kender Romel Jean Charles <makender103@gmail.com>
2023-07-25 20:49:59 -0300

    feat: cart feature, adicionando produto ao carinho

--- Project/backend/dist/controllers/cart-controller.js
+++ Project/backend/dist/controllers/cart-controller.js
@@ -1,7 +1,30 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const response_dto_1 = require("../dtos/response.dto");
+const api_logger_1 = require("../utils/api-logger");
+const cart_action_1 = require("../actions/cart-action");
 class CartController {
     create(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const response = yield cart_action_1.default.create(req);
+                api_logger_1.default.error(`[CartController] - create - body - ${JSON.stringify(req.body)}`);
+                response_dto_1.default.httpSuccessResponse(res, 200, response);
+            }
+            catch (error) {
+                api_logger_1.default.error(`[CartController] - create - error - ${JSON.stringify(error)}`);
+                response_dto_1.default.httpErrorResponse(res, error);
+            }
+        });
     }
 }
 exports.default = new CartController();
--- Project/backend/src/controllers/cart-controller.ts
+++ Project/backend/src/controllers/cart-controller.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from "express";
 import ResponseDto from "../dtos/response.dto";
 import apiLogger from "../utils/api-logger";
+import CartAction from "../actions/cart-action";
 
 class CartController {
-    create(req: Request, res: Response) {
-       
+    async create(req: Request, res: Response) {
+        try {
+            const response = await CartAction.create(req);
+            apiLogger.error(`[CartController] - create - body - ${JSON.stringify(req.body)}`);
+            ResponseDto.httpSuccessResponse(res, 200, response);
+        } catch (error) {
+            apiLogger.error(`[CartController] - create - error - ${JSON.stringify(error)}`);
+            ResponseDto.httpErrorResponse(res, error);
+        }
     }
 }
 
