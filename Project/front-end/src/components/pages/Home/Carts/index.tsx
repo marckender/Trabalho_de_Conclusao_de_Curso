9eb6adada2800db94c7bb6e403d0e3e6644ab0b8
marckender <makender103@gmail.com>
2023-12-10 23:39:35 -0300

    chore: deploy issue

--- Project/front-end/src/components/pages/Home/Carts/index.tsx
+++ Project/front-end/src/components/pages/Home/Carts/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { PageDefault } from '../PageDefault';
 import { useCartContext } from '../../../../contexts/useCartContext';
 
